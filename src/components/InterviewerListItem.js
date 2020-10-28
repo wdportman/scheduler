8e8ecdfd21e7dd7241d5c9906da1f1f8fac9119f
Will <wdportman@gmail.com>
2020-10-27 18:35:18 -0700

    Create InterviewerListItem component

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -5,14 +5,19 @@ import "components/styling/InterviewerListItem.scss";
 
 export default function InterviewerListItem(props) {
   const { name, avatar, selected, setInterviewer } = props;
+  const InterviewerListItemClass = classNames({
+    'interviewers__item': true,
+    'interviewers__item--selected': selected
+  });
   return (
-    <li className="interviewers__item">
+    <li className={InterviewerListItemClass}
+    onClick={setInterviewer}>
       <img
         className="interviewers__item-image"
         src={avatar}
         alt={name}
       />
-      {name}
+      {selected && name}
     </li>
   );
 }
