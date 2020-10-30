7cf685e23afb503b43f6e39efee1157160ac73ac
Will <wdportman@gmail.com>
2020-10-29 21:40:05 -0700

    Fix bug in getInterview function where data wasn't persisting after one load

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -20,16 +20,19 @@ const output = [];
 };
 
 const getInterview = (state, interview) => {
-  let output = null;
-  if (interview) {
-    for (const interviewer in state.interviewers) {
-      if (state.interviewers[interviewer].id === interview.interviewer) {
-        interview.interviewer = state.interviewers[interviewer];
-        output = interview;
-      }
+  let output = {};
+  if (!interview) {
+    return null;
+  }
+  output.student = interview.student;
+  output.interviewer = {id: interview.interviewer};
+  for (const interviewer in state.interviewers) {
+    if (state.interviewers[interviewer].id === interview.interviewer) {
+      output["interviewer"] = {...output.interviewer, name: state.interviewers[interviewer].name};
+      output["interviewer"] = {...output.interviewer, avatar: state.interviewers[interviewer].avatar};
     }
   }
   return output;
-};
+}
 
 export { getAppointmentsForDay, getInterview }
