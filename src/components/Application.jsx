aa96a263e4cc03d4d93286cfa66f64fdaebb61fa
Will <wdportman@gmail.com>
2020-10-30 21:47:05 -0700

    Move functions from Application.jsx into useApplicationData.js hook

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,57 +1,15 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
 
 import "components/styling/Application.scss";
+
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
 import { getAppointmentsForDay, getInterviewersForDay, getInterview } from "../helpers/selectors.js";
+import { useApplicationData } from "../hooks/useApplicationData.js";
 
 export default function Application(props) {
 
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {
-      "1": {
-        id: 1,
-        time: "12pm",
-        interview: null
-      }
-    },
-    interviewers: {}
-  });
-
-  const setDay = (day) => setState({ ...state, day });
-
-  function bookInterview(id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-    return axios.put(`/api/appointments/${id}`, appointment)
-      .then(() => {
-        setState({...state, appointments});
-      })
-      .then()
-  };
-
-  function cancelInterview(id) {
-    return axios.delete(`api/appointments/${id}`)
-  }
-  
-  useEffect(() => {
-    Promise.all([
-      Promise.resolve(axios.get('/api/days')),
-      Promise.resolve(axios.get('/api/appointments')),
-      Promise.resolve(axios.get('/api/interviewers'))
-    ]).then((all) => {
-      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
-    });
-  }, [])
+  const { state, setDay, bookInterview, cancelInterview } = useApplicationData();
 
   const appointments = getAppointmentsForDay(state, state.day).map(appointment => {
     const {id, time} = appointment;
---
+++ src/hooks/useApplicationData.js
@@ -0,0 +1,52 @@
+import { useState, useEffect } from "react";
+import axios from 'axios';
+
+const useApplicationData = () => {
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {
+      "1": {
+        id: 1,
+        time: "12pm",
+        interview: null
+      }
+    },
+    interviewers: {}
+  });
+
+  useEffect(() => {
+    Promise.all([
+      Promise.resolve(axios.get('/api/days')),
+      Promise.resolve(axios.get('/api/appointments')),
+      Promise.resolve(axios.get('/api/interviewers'))
+    ]).then((all) => {
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+    })
+  }, []);
+
+  const setDay = (day) => setState({ ...state, day });
+
+  function bookInterview(id, interview) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    return axios.put(`/api/appointments/${id}`, appointment)
+      .then(() => {
+        setState({ ...state, appointments });
+      })
+  };
+
+  function cancelInterview(id) {
+    return axios.delete(`api/appointments/${id}`)
+  };
+  return { state, setDay, bookInterview, cancelInterview };
+};
+
+export { useApplicationData }
