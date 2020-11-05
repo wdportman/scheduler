958eaf01c3a6806a538582eec8713a3415a9220e
Will <wdportman@gmail.com>
2020-11-04 19:01:52 -0800

    Create tests for error messages

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,15 +1,18 @@
+//NOTE: I split Application tests into two files, Application.test.js and Application2.test.js. Some tests were failing, apparently because the state was changed in one test and not cleaned up in the following test despite using "afterEach(cleanup)." After lots of googling, I debugged this with a mentor for a while, and the simplest solution ended up being to split the tests into two files.
+
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByAltText, getByPlaceholderText, queryByText } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, getByText, getAllByTestId, getByAltText, getByPlaceholderText, queryByText } from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
 describe("Application", () => {
+
   it("changes the schedule when a new day is selected", async () => {
     const { getByText } = render(<Application />);
-  
+
     await waitForElement(() => getByText("Monday"));
   
     fireEvent.click(getByText("Tuesday"));
@@ -17,7 +20,8 @@ describe("Application", () => {
     expect(getByText("Leopold Silvers")).toBeInTheDocument();
   });
 
-  it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
+
+  it("loads data, books an interview, and reduces the spots remaining for Monday by 1", async () => {
     const { container } = render(<Application/>);
 
     await waitForElement(() => getByText(container, "Archie Cohen"));
@@ -40,7 +44,8 @@ describe("Application", () => {
     expect(queryByText(day, "no spots remaining")).toBeInTheDocument();
   });
 
-  it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+
+  it("loads data, cancels an interview, and increases the spots remaining for Monday by 1", async () => {
     const { container } = render(<Application/>);
 
     await waitForElement(() => getByText(container, "Archie Cohen"));
@@ -48,7 +53,7 @@ describe("Application", () => {
     const appointment = getAllByTestId(container, "appointment").find(
       appointment => queryByText(appointment, "Archie Cohen")
     );
-    
+
     fireEvent.click(getByAltText(appointment, "Delete"));
 
     expect(getByText(appointment, "Are you sure you want to delete this appointment?")).toBeInTheDocument();
---
+++ src/components/__tests__/Application2.test.js
@@ -0,0 +1,97 @@
+//NOTE: I split Application tests into two files, Application.test.js and Application2.test.js. Some tests were failing, apparently because the state was changed in one test and not cleaned up in the following test despite using "afterEach(cleanup)." After lots of googling, I debugged this with a mentor for a while, and the simplest solution ended up being to split the tests into two files.
+
+import React from "react";
+
+//The below is to import the mock Axios library from the __mocks__ folder:
+import axios from "axios";
+
+import { render, cleanup, waitForElement, fireEvent, getByText, getAllByTestId, getByAltText, getByPlaceholderText, queryByText } from "@testing-library/react";
+
+import Application from "components/Application";
+
+afterEach(cleanup);
+
+describe("Application", () => {
+
+  it("loads data, edits an interview, and keeps the spots remaining for Monday the same", async () => {
+
+    const { container } = render(<Application />);
+
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    const appointment = getAllByTestId(container, "appointment").find(
+      appointment => queryByText(appointment, "Archie Cohen")
+    );
+
+    fireEvent.click(getByAltText(appointment, "Edit"));
+
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Will Portman" }
+    });
+
+    fireEvent.click(getByText(appointment, "Save"));
+
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+
+    await waitForElement(() => queryByText(appointment, "Will Portman"));
+
+    const day = getAllByTestId(container, "day").find(day =>
+      queryByText(day, "Monday")
+    );
+    expect(queryByText(day, "1 spot remaining")).toBeInTheDocument();
+  });
+
+
+  it("shows the save error when failing to save an appointment", async () => {
+
+    axios.put.mockRejectedValueOnce();
+
+    const { container } = render(<Application />);
+
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    const appointment = getAllByTestId(container, "appointment")[0];
+
+    fireEvent.click(getByAltText(appointment, "Add"));
+
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+    fireEvent.click(getByText(appointment, "Save"));
+
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+    await waitForElement(() => getByText(appointment, "Oops! There was an error saving your appointment."));
+    expect(getByText(appointment, "Oops! There was an error saving your appointment.")).toBeInTheDocument();
+
+  });
+
+
+  it("shows the save error when failing to delete an appointment", async () => {
+
+    axios.delete.mockRejectedValueOnce();
+
+    const { container } = render(<Application />);
+
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    const appointment = getAllByTestId(container, "appointment").find(
+      appointment => queryByText(appointment, "Archie Cohen")
+    );
+
+    fireEvent.click(getByAltText(appointment, "Delete"));
+
+    expect(getByText(appointment, "Are you sure you want to delete this appointment?")).toBeInTheDocument();
+
+    fireEvent.click(getByText(appointment, "Confirm"));
+
+    expect(getByText(appointment, "Deleting")).toBeInTheDocument();
+    await waitForElement(() => getByText(appointment, "Oops! There was an error deleting your appointment."));
+    expect(getByText(appointment, "Oops! There was an error deleting your appointment.")).toBeInTheDocument();
+
+
+  });
+
+});
