4623da861ca7ceddaec3ace7fd1b62b3d07a88f2
Will <wdportman@gmail.com>
2020-11-08 21:56:47 -0800

    Correct file name of appointment.scss

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -8,7 +8,7 @@ import Status from 'components/Appointment/Status.jsx';
 import Confirm from 'components/Appointment/Confirm.jsx';
 import Error from 'components/Appointment/Error.jsx';
 
-import "../styling/Appointment.scss";
+import "../styling/appointment.scss";
 
 import { useVisualMode } from "../../hooks/useVisualMode.js";
 
